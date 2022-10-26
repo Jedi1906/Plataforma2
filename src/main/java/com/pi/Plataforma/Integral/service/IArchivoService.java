a39c5e9c0f9a1f5e44d7f81b4b6e4bea19bf9cfd
marisolhernandezparedes <86931376+marisolhernandezparedes@users.noreply.github.com>
2022-10-26 11:56:33 -0500

    Delete src/main/java/com/pi/Plataforma/Integral/service directory

--- src/main/java/com/pi/Plataforma/Integral/service/IArchivoService.java
+++
@@ -1,9 +0,0 @@
-package com.pi.Plataforma.Integral.service;
-
-import com.pi.Plataforma.Integral.models.Archivo;
-
-import java.util.List;
-
-public interface IArchivoService {
-    List<Archivo> getAll();
-}
